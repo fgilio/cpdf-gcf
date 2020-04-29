3340461aae4bf7d98fdbb7397f4ae040d3fee77a
Franco Gilio <fgilio@users.noreply.github.com>
2020-04-29 09:11:18 -0300

    Make compatible with GCP Node.js 10 runtime

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { PATH, CODE_LOCATION } = process.env
+const { PATH, PWD } = process.env
 
 const BIN = 'node_modules/cpdf-gcf/bin'
 
-process.env.PATH = `${PATH}:${CODE_LOCATION}/${BIN}`
+process.env.PATH = `${PATH}:${PWD}/${BIN}`
 
 module.exports.version = () => {
   return new Promise((resolve, reject) => {
